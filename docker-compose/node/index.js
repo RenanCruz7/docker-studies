b52519ae0e49b66c0a77ba49717cda2d3220859d
RenanCruz7 <renancruz199@gmail.com>
2025-06-07 12:20:07 -0300

    compose watch

--- docker-compose/node/index.js
+++ docker-compose/node/index.js
@@ -2,8 +2,8 @@ const mongoose = require('mongoose');
 
 mongoose.connect("mongodb://mongo:27017/test", {useNewUrlParser: true})
     .then(() => {
-        console.log("Connected to MongoDB");
+        console.log("Connected to MongoDB!!!!!!!!!!!!!!!!!!!!!");
     })
     .catch(err => {
-        console.error("Error connecting to MongoDB", err);
+        console.error("Error connecting to MongoDB !!!!!!", err);
     });
